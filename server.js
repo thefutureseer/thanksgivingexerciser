a1bd341a6c4c557e83a6f9d8d03c773f3411b69b
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2020-01-18 12:54:25 -0800

    version 1.0.0

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+//serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+//set static folder
+ app.use(express.static('thanksgiving-exercise-tracker/build'));
+ app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'thanksgiving-exercise-tracker', 'build', 'index.html'));
+ });
+} 
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
