16778d7eb48464a34e442f1f00a124d08feb3f5c
thefutureseer <danielharrislimos@outlook.com>
2023-03-29 12:27:42 -0700

    stashing

---
+++ client/src/components/create-user.component.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function CreateUser() {
+
+  
+  const initialState = {
+    username: '',
+  };
+  
+  const [userState, setUser] = useState(initialState);
+  
+  
+  const onChangeUsername = (e) => {
+    setUser({
+      ...userState,
+      username: e.target.value
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      username: userState.username
+    };
+
+    //  console.log(user);
+
+    axios.post('http://localhost:5000/users/add', user)
+      .then(res => console.log(res.data));
+        setUser({
+          username: ''
+        })
+  }
+
+    return (
+      <div>
+        <h3>Create New User</h3>
+        <form onSubmit={onSubmit}>
+          <div className="form-group"> 
+            <label>Username: </label>
+            <input  type="text"
+                required
+                className="form-control"
+                value={userState.username}
+                onChange={onChangeUsername}
+                />
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Create User" className="btn btn-primary" />
+          </div>
+        </form>
+      </div>
+    )
+}
