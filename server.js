5406d3b9028acf55e74d8519504a9c5cd9d17bc1
thefutureseer <danielharrislimos@outlook.com>
2024-07-31 02:56:06 -0700

    updates and add proxy for development

--- server.js
+++ server.js
@@ -30,9 +30,13 @@ if(process.env.NODE_ENV === 'production') {
 //set static folder
  app.use(express.static('client/build'));
  app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
- });
-} 
+   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+} else {
+   app.get('/',(req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'client'))
+  });
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
