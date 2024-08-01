f98e0b74871522338314f1d0e148f5d774d0f698
thefutureseer <danielharrislimos@outlook.com>
2024-08-01 01:38:30 -0700

    delete else block

--- server.js
+++ server.js
@@ -32,10 +32,6 @@ if(process.env.NODE_ENV === 'production') {
  app.get('*', (req, res) => {
    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-} else {
-   app.get('/',(req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'client'))
-  });
 }
 
 app.listen(port, () => {
