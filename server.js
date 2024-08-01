2b62a13b2792eef3f2a788108c86fe731cafc355
thefutureseer <danielharrislimos@outlook.com>
2024-08-01 01:00:47 -0700

    add dirname

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/users', usersRouter);
 //serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
 //set static folder
- app.use(express.static('client/build'));
+ app.use(express.static(path.join(__dirname, 'client', 'build')));
  app.get('*', (req, res) => {
    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
